938252b2169c296798ce675d9b2e6d40903c66bd
Miriam Anyango <minaoanya@gmail.com>
2022-06-15 11:02:48 +0300

    commit tree 5587955fa2bef4fa9a097b81c67f4174c20f0e99

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers,driver){
+    return drivers.filter(userMatch=>userMatch.toLowerCase()===driver.toLowerCase())
+    
+
+}
+function fuzzyMatch(drivers,driver){
+    return drivers.filter(letterMatch=>letterMatch.toLowerCase().indexOf(driver.toLowerCase())===0)
+
+}
+function matchName(drivers,driver){
+    return drivers.filter(nameMatch=>nameMatch.name===driver)
+
+}
+
+const students= ["mary","john","mary","sharon","victor"]
+// console.log(findMatching(students,"mary"))
+console.log(fuzzyMatch(students,"v"))
+
+// const students= ["mary","john","mary","sharon","victor"]
+// console.log(students.indexOf("mary")); 
